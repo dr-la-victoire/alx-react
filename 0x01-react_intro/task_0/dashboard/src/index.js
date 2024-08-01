13699f47fc81662b649d7fcb239407c1f54414de
Victory Amadi <lavictoire633@gmail.com>
2024-08-01 16:49:46 +0100

    adding the Notification component

--- 0x01-react_intro/task_0/dashboard/src/index.js
+++ 0x01-react_intro/task_0/dashboard/src/index.js
@@ -1,6 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import Notifications from "./Notifications";
 
 const inject = document.getElementById("root");
-ReactDOM.render(<App />, inject);
+ReactDOM.render(
+  <div className="main">
+    <div className="root-notifications">
+      <Notifications />
+    </div>
+    <App />
+  </div>,
+  inject
+);
