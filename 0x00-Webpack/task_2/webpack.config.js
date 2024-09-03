8cf4eb9be1f716b4182936b55b877f3f8ea14f71
La Victoire <lavictoire633@gmail.com>
2024-09-03 16:52:11 +0100

    adding loaders to support css and images in webpack

--- 0x00-Webpack/task_2/webpack.config.js
+++ 0x00-Webpack/task_2/webpack.config.js
@@ -3,23 +3,33 @@ const path = require("path");
 module.exports = {
   mode: "production",
   entry: {
-    main: path.resolve(__dirname, "./js/dashboard_main.js"),
+    main: path.resolve(__dirname, "./js/dashboard_main.js")
   },
   output: {
     path: path.resolve(__dirname, "public"),
-    filename: "bundle.js",
+    filename: "bundle.js"
+  },
+  performance: {
+    maxAssetSize: 1000000
   },
   module: {
     rules: [
       {
         test: /\.css$/,
-        use: ["style-loader", "css-loader"],
+        use: ["style-loader", "css-loader"]
       },
       {
-        test: /\.(gif|png|jpg|jpeg)$/i,
-        use: ["file-loader"],
-        type: "asset/resource",
-      },
-    ],
-  },
+        test: /\.(gif|svg|png|jpg|jpeg)$/i,
+        use: [
+          {
+            loader: ["file-loader", "image-webpack-loader"],
+            options: {
+              bypassOnDebug: true,
+              disable: true
+            }
+          }
+        ]
+      }
+    ]
+  }
 };
