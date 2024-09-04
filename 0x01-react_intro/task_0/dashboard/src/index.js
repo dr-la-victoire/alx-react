c0afcc22af6305f115613c36087485204880f1e1
La Victoire <lavictoire633@gmail.com>
2024-09-04 08:42:15 +0100

    adding files for task 1

--- 0x01-react_intro/task_0/dashboard/src/index.js
+++ 0x01-react_intro/task_0/dashboard/src/index.js
@@ -1,15 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "./App.css";
 import App from "./App";
-import Notifications from "./Notifications";
 
-const inject = document.getElementById("root");
-ReactDOM.render(
-  <div className="main">
-    <div className="root-notifications">
-      <Notifications />
-    </div>
-    <App />
-  </div>,
-  inject
-);
+const root = document.getElementById("root");
+ReactDOM.render(<App />, root);
