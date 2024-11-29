0ca6f6e750fd30b236e7890233bd42e80ac20844
La Victoire <lavictoire633@gmail.com>
2024-11-29 17:32:59 +0100

    setting up a reducer function for the course selected

---
+++ 0x08-react_redux_reducer_selector/task_2/dashboard/src/actions/courseActionTypes.js
@@ -0,0 +1,5 @@
+export const SELECT_COURSE = "SELECT_COURSE";
+export const UNSELECT_COURSE = "UNSELECT_COURSE";
+
+// Creating the new action for when the API returns a list of courses
+export const FETCH_COURSE_SUCCESS = "FETCH_COURSE_SUCCESS";
