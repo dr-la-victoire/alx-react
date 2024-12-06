1796a06fb5f51af5705be76fc8de78e034a56dc5
La Victoire <lavictoire633@gmail.com>
2024-12-06 15:10:17 +0100

    creating the store

--- 0x09-react_redux_connectors_and_providers/task_0/dashboard/src/index.js
+++ 0x09-react_redux_connectors_and_providers/task_0/dashboard/src/index.js
@@ -1,10 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App/App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App/App";
+import { createStore } from "redux";
+import uiReducer, { defaultState } from "./reducers/uiReducer";
+import { Map } from "immutable";
+import { Provider } from "react-redux";
+
+const theStore = createStore(uiReducer, Map(defaultState));
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={theStore}>
+      <App />
+    </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
